391924f5f27e599ba639b66e90a0b68681c41578
Emanuel de Melo Pegado <20251011110019@ifrn.local>
2026-01-29 10:30:10 -0300

    alteração nos butões da tela inicial

--- lobo.py
+++ lobo.py
@@ -16,7 +16,14 @@ y = 200
 background = pygame.image.load("start.png").convert()
 background = pygame.transform.scale(background, (1280, 720))
 
-start_rect = pygame.Rect(520, 260, 240, 80)            
+sair_rect  = pygame.Rect(520, 310, 240, 60)
+start_rect = pygame.Rect(520, 240, 240, 60)
+fonte = pygame.font.SysFont(None, 60)
+
+COR_BOTAO = (70, 130, 180)
+COR_HOVER = (100, 160, 210)
+COR_TEXTO = (255, 255, 255)
+
 
 while running:
     for event in pygame.event.get():
@@ -27,24 +34,53 @@ while running:
             if event.button == 1:  
                 if start_rect.collidepoint(event.pos):
                  estado="jogo"
+                if sair_rect.collidepoint(event.pos):
+                 running = False
+    if estado == "menu":
+       screen.blit(background, (0, 0))
+       mouse_pos = pygame.mouse.get_pos()
 
-        if estado == "menu":
-           screen.blit(background, (0, 0))
+       if start_rect.collidepoint(mouse_pos):
+         pygame.draw.rect(screen, COR_HOVER, start_rect, border_radius=12)
+       else:
+         pygame.draw.rect(screen, COR_BOTAO, start_rect, border_radius=12)
 
-        elif estado == "jogo":
-          screen.fill((255, 255, 255))
-          screen.blit(quadrado, (x, y))
-        pygame.display.flip()
+         texto = fonte.render("START", True, COR_TEXTO)
+         texto_rect = texto.get_rect(center=start_rect.center)
+         screen.blit(texto, texto_rect)
+         
+       if sair_rect.collidepoint(mouse_pos):
+         pygame.draw.rect(screen, COR_HOVER, sair_rect, border_radius=12)
+       else:
+         pygame.draw.rect(screen, COR_BOTAO, sair_rect, border_radius=12)
+
+         texto_sair = fonte.render("SAIR", True, COR_TEXTO)
+         screen.blit(texto_sair, texto_sair.get_rect(center=sair_rect.center))
+
+    elif estado == "jogo":
+       screen.fill((0, 150, 0))
+       screen.blit(quadrado, (x, y))
+    pygame.display.flip()
     teclas = pygame.key.get_pressed() # Para capturar o pressionamento das teclas de forma contínua
-    if teclas[pygame.K_LEFT]:   # tecla direcional esquerda está sendo pressionada?
-      x = x - velocidade
-    if teclas[pygame.K_RIGHT]:  # tecla direcional direita está sendo pressionada?
-      x = x + velocidade
-    if teclas[pygame.K_UP]:
-      y = y - velocidade 
-    if teclas[pygame.K_DOWN]:
-       y = y + velocidade      
+    if estado == "jogo":
+       if teclas[pygame.K_LEFT]:   # tecla direcional esquerda está sendo pressionada?
+         x = x - velocidade
+       if teclas[pygame.K_RIGHT]:  # tecla direcional direita está sendo pressionada?
+         x = x + velocidade
+       if teclas[pygame.K_UP]:
+         y = y - velocidade 
+       if teclas[pygame.K_DOWN]:
+         y = y + velocidade 
+       if x < 0:
+         x = 0
+       if x > 1280 - 30:
+         x = 1280 - 30
+
+       if y < 0:
+         y = 0
+       if y > 720 - 30:
+         y = 720 - 30       
      
-clock.tick(60)
+    clock.tick(60)
 
 pygame.quit()
